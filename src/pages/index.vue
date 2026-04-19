bc1e3041dab6cca98843ef86efa8e2df4899c8a0
notcikken <cyrullusikkend@student.uns.ac.id>
2026-04-19 19:17:00 +0700

    feat: add CTASection component with engaging freelance experience content

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import LandingLayout from '@/layouts/UserLayout.vue'
 import Hero from '@/components/Hero.vue'
+import CTASection from '@/components/CTASection.vue'
 
 defineOptions({
   name: 'HomePage',
@@ -10,5 +11,6 @@ defineOptions({
 <template>
   <LandingLayout>
     <Hero />
+    <CTASection />
   </LandingLayout>
 </template>

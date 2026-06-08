78105f504a2708110387c3a1bade183e29e95a6e
Ade Kurniawan <145414449+adkurnwn@users.noreply.github.com>
2026-06-08 21:54:20 +0700

    fix: treats to unverified candidate

--- src/components/UserManagementTabs.vue
+++ src/components/UserManagementTabs.vue
@@ -9,5 +9,6 @@ const activeTab = defineModel<string>({ default: 'Admin' })
     <n-tab-pane name="Admin" tab="Admin" />
     <n-tab-pane name="Employee" tab="Pegawai" />
     <n-tab-pane name="Candidate" tab="Kandidat" />
+    <n-tab-pane name="Unverified" tab="Unverified" />
   </n-tabs>
 </template>
